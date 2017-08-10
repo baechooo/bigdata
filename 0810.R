50e84b442f65fa797bfaad06b535fc96f6dc2ffa
baechooo <bjy1278@gmail.com>
2017-08-10 14:04:25 +0900

    summary
    changed1

--- 0810.R
+++ 0810.R
@@ -1,15 +1,19 @@
-a <- c(1,1)
+data("Auto")
+summary(Auto)
+head(Auto)
+attach(Auto)
 
-1:10
+lm.fit = lm(mpg ~ horsepower)
+summary(lm.fit)
 
-afun <- function(x,y)
-{
-  v <- x + y
-  return(v)
-}
 
-v<-0
-for (iiii in (1:100))
-{
-  v <- iiii + v
-}
+#9(a)
+pairs(Auto)
+
+#9(b)
+# Compute the matrix of correlations between the variables using the function cor(). 
+# You will need to exclude the name variable, which is qualitative.
+
+cor(subset(Auto, select = -name))
+lm.fit1 = lm(mpg~.-name, data = Auto)
+summary(lm.fit1)
